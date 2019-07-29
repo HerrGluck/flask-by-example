cee274ae40dccd3b48fa8d47d0f0f7bbcf5fdc1a
HerrGluck <pochta4me00@gmail.com>
2019-07-29 21:05:45 +0300

    File Uploaded success

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-#print(os.environ['APP_SETTINGS'])
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+# print(os.environ['APP_SETTINGS'])
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/upload-image')
+def upload_show():
+    return render_template("upload_image.html")
+
+
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        image = request.files['image']
+        target = os.path.join(APP_ROOT, 'image_uploads/')
+        print(target)
+        destination = "/".join([target, image.filename])
+        print(destination)
+        image.save(destination)
+        print('Image saved')
+    return render_template("uploaded.html")
 
 
 if __name__ == '__main__':
